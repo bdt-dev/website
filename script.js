5431f08621998d7c32baa1ea92301597309953ea
bdtdev <>
2021-05-22 00:46:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+function sayHi() {
+  document.getElementById('response').innerHTML += "<img class='small-image' src='https://images.unsplash.com/photo-1508313880080-c4bef0730395?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80' />"
+}
